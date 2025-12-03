32b693ea65d6b07270b26090fe9a1a5e71ef280a
Lucas Jacob Gill <hello@lucasgill.dev>
2025-12-03 17:22:39 +0000

    feat: experimental viewTransitions API added to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { ViewTransition } from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -34,9 +35,11 @@ export default function RootLayout({
       <body
         className={`${geist.variable} ${geistMono.variable} antialiased min-h-svh flex flex-col`}
       >
-        <Header />
-        <main className="flex-1">{children}</main>
-        <Footer />
+        <ViewTransition>
+          <Header />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </ViewTransition>
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
